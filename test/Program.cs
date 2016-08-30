9e4e1d963f2514ee540c4ef39f19808dbed30498
Hazemax <zehd@bk.ru>
2016-08-30 23:56:08 +0300

    Test

---
+++ test/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using SocksHttpWeb;
+using System.Net;
+namespace test
+{
+    class Program
+    {
+        static string WebMethodSocks(string uri)
+        {
+            WebRequest req = SocksHttpWebRequest.Create(uri);
+            req.Proxy = new WebProxy(new Uri("socks5://192.168.0.231:1080"));
+            string res;
+            using (WebResponse resp = req.GetResponse())
+            using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+                res = sr.ReadToEnd();
+
+            return res;
+        }
+
+        static string HttpWebMethodSocks(string uri)
+        {
+            HttpWebRequest req = (HttpWebRequest)SocksHttpWebRequest.Create(uri);
+            req.Proxy = new WebProxy(new Uri("socks5://192.168.0.231:1080"));
+            string res;
+            using (HttpWebResponse resp = (HttpWebResponse)req.GetResponse())
+            using (StreamReader sr = new StreamReader(resp.GetResponseStream()))
+                res = sr.ReadToEnd();
+
+            return res;
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(WebMethodSocks("http://google.com"));
+
+            Console.WriteLine(HttpWebMethodSocks("http://google.com"));
+
+            Console.ReadKey();
+        }
+    }
+}
